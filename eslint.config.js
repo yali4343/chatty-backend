9cce43c42cb8b38b8017862ef44d335fd4c6c4b4
yali4343 <yali4343@gmail.com>
2026-09-13 20:24:14 +0300

    setup project

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const prettierConfig = require('eslint-config-prettier');
+
+module.exports = [
+  {
+    ignores: ['node_modules/**', 'build/**'],
+  },
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      semi: ['error', 'always'],
+      'space-before-function-paren': 'off',
+      camelcase: 'off',
+      'no-return-assign': 'off',
+      quotes: ['error', 'single'],
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+  },
+  prettierConfig,
+];
